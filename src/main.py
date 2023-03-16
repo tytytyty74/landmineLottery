39214284c64157ad3018b5348cc8b31298b00284
Stephane Katende <swar.kung@gmail.com>
2023-03-16 17:00:02 -0400

    added pygame + show a red square

--- src/main.py
+++ src/main.py
@@ -2,15 +2,18 @@ from grid import Grid
 import pygame
 from sys import exit
 
-
-pygame.int()
+# pygame setup stuff
+pygame.init()
 srn = pygame.display.set_mode((1600, 900))
 pygame.display.set_caption('LandMineLottery')
 clock = pygame.time.Clock()
-x = Grid(10, 10)
 
+#surface for the bomb, a red square basically
 bomb_surface = pygame.Surface((50,50))
 bomb_surface.fill('Red')
+
+# grid setup stuff
+x = Grid(10, 10)
 
 while True:
         for e in pygame.event.get():
