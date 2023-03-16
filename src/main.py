18831bb1ef72ded1df99e3dfb7783e156f2d3543
Stephane Katende <swar.kung@gmail.com>
2023-03-16 16:52:15 -0400

    adding pygame + displaying a screen

--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
 from grid import Grid
+import pygame
+from sys import exit
 
+
+pygame.int()
+srn = pygame.display.set_mode((1600, 900))
+pygame.display.set_caption('LandMineLottery')
+clock = pygame.time.Clock()
 x = Grid(10, 10)
 
+bomb_surface = pygame.Surface((50,50))
+bomb_surface.fill('Red')
 
-print(x)
+while True:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        srn.blit(bomb_surface, (200 , 100))
+        pygame.display.update()
+        clock.tick(30)
